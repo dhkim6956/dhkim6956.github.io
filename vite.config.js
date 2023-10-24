687f0d8794acd617dce4a6c706508595b9a5e87c
dhkim6956 <dhkim6956@naver.com>
2023-10-25 00:40:47 +0900

    Deploy Test 2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-
-const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.join(__dirname, "docs")
-  }
 })
